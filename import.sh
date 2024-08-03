0fae5f5adc442c863a70fd283a4f96f4f3d81b91
Nikhil Mathew Francis <62801039+nikhilmathewfrancis98@users.noreply.github.com>
2024-08-03 18:26:54 +0530

    Create import.sh to import the resource group
    Advised by GPT

---
+++ import.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+RESOURCE_GROUP_NAME="kubernetes_dev"
+RESOURCE_GROUP_ID="/subscriptions/80113cca-9d1e-4436-ba96-6235136ee2cf/resourceGroups/${RESOURCE_GROUP_NAME}"
+
+# Initialize Terraform
+terraform init
+
+# Import the existing resource group into Terraform state
+terraform import azurerm_resource_group.resource_group ${RESOURCE_GROUP_ID}
