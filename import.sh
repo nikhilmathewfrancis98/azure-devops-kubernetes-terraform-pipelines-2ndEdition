c05889afbe79b96b8682e953d452253e16606d2b
Nikhil Mathew Francis <62801039+nikhilmathewfrancis98@users.noreply.github.com>
2024-08-03 18:55:03 +0530

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change to the working directory where your Terraform files are located
+cd "$(System.DefaultWorkingDirectory)/configuration/iaac/azure/kubernetes"
+
 # Variables
 RESOURCE_GROUP_NAME="kubernetes_dev"
 RESOURCE_GROUP_ID="/subscriptions/80113cca-9d1e-4436-ba96-6235136ee2cf/resourceGroups/${RESOURCE_GROUP_NAME}"
